b27324e683ddcb139777a79c81c7220717b72c5e
lsp_lishuiping@qq.com <lsp_lishuiping@qq.com>
2024-11-08 08:50:08 +0800

    重构编辑器代码

--- lexical-editor/src/App.jsx
+++ lexical-editor/src/App.jsx
@@ -4,16 +4,45 @@ import RichEditor from './RichEditor';
 export default function App() {
     const editorRef = React.createRef(undefined);
 
+    function onError(error) {
+        console.error(error);
+    }
+
+    function onChange(editorState) {
+
+    }
+
     const exportHtml = () => {
         const htmlString = editorRef.current.exportHtml();
         console.log(htmlString);
     };
 
+    const htmlString = `<p class="editor-paragraph" dir="ltr">
+  <span style="white-space: pre-wrap;">paragraph</span>
+</p>
+<p class="editor-paragraph" dir="ltr">
+  <b>
+    <strong class="editor-text-bold" style="white-space: pre-wrap;">heading</strong>
+  </b>
+</p>
+<p class="editor-paragraph" dir="ltr">
+  <span style="white-space: pre-wrap;">quote</span>
+</p>
+<p class="editor-paragraph" dir="ltr" style="padding-inline-start: 80px;">
+  <span style="white-space: pre-wrap;">paragraph</span>
+</p>
+<p class="editor-paragraph" dir="ltr">
+  <span style="white-space: pre-wrap;">heading</span>
+</p>
+<p class="editor-paragraph" dir="ltr">
+  <span style="white-space: pre-wrap;">quote</span>
+</p>`;
+
     return (
         <>
             <button onClick={exportHtml}>Export</button>
 
-            <RichEditor ref={editorRef} />
+            <RichEditor ref={editorRef} placeholder='Enter some rich text...' value={htmlString} onError={onError} onChange={onChange} />
         </>
     );
 }
--- lexical-editor/src/RichEditor/index.jsx
+++ lexical-editor/src/RichEditor/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useImperativeHandle } from 'react';
+import React, { useImperativeHandle } from 'react';
 import { $generateNodesFromDOM } from '@lexical/html';
 import { AutoFocusPlugin } from '@lexical/react/LexicalAutoFocusPlugin';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
@@ -17,51 +17,25 @@ import HtmlPlugin from './HtmlPlugin';
 import './index.css';
 
 function RichEditor(props, ref) {
+    const { placeholder, value, onError, onChange } = props;
     const htmlPluginRef = React.createRef();
-    const [editorState, setEditorState] = useState();
 
-    function onError(error) {
-        console.error(error);
-    }
-
-    function onChange(editorState) {
-        // Call toJSON on the EditorState object, which produces a serialization safe string
-        const editorStateJSON = editorState.toJSON();
-        // However, we still have a JavaScript object, so we need to convert it to an actual string with JSON.stringify
-        setEditorState(JSON.stringify(editorStateJSON));
-    }
-
-    function initText(editor) {
-        const parser = new DOMParser();
-        const htmlString = `<p class="editor-paragraph" dir="ltr">
-  <span style="white-space: pre-wrap;">paragraph</span>
-</p>
-<p class="editor-paragraph" dir="ltr">
-  <b>
-    <strong class="editor-text-bold" style="white-space: pre-wrap;">heading</strong>
-  </b>
-</p>
-<p class="editor-paragraph" dir="ltr">
-  <span style="white-space: pre-wrap;">quote</span>
-</p>
-<p class="editor-paragraph" dir="ltr" style="padding-inline-start: 80px;">
-  <span style="white-space: pre-wrap;">paragraph</span>
-</p>
-<p class="editor-paragraph" dir="ltr">
-  <span style="white-space: pre-wrap;">heading</span>
-</p>
-<p class="editor-paragraph" dir="ltr">
-  <span style="white-space: pre-wrap;">quote</span>
-</p>`;
-        const dom = parser.parseFromString(htmlString, 'text/html');
-
-        const nodes = $generateNodesFromDOM(editor, dom);
+    function initEditorState(editor) {
         const rootNode = $getRoot();
-        for (const node of nodes) {
-            try {
-                rootNode.append(node);
-            } catch (err) {
-                continue;
+
+        if (value?.length > 0) {
+            const parser = new DOMParser();
+            const htmlString = value;
+            const dom = parser.parseFromString(htmlString, 'text/html');
+
+            const nodes = $generateNodesFromDOM(editor, dom);
+
+            for (const node of nodes) {
+                try {
+                    rootNode.append(node);
+                } catch (err) {
+                    continue;
+                }
             }
         }
 
@@ -79,12 +53,11 @@ function RichEditor(props, ref) {
     }));
 
     const initialConfig = {
-        editorState: initText,
+        editorState: initEditorState,
         namespace: 'MyEditor',
         theme: ExampleTheme,
         onError,
     };
-    const placeholder = 'Enter some rich text...';
 
     return (
         <LexicalComposer initialConfig={initialConfig}>
